d63512cb4c806b329b2fc807a510a25bb967eb90
maxirozay <rossier.maxime@gmail.com>
2016-11-11 12:31:38 +0100

    create the contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,42 @@
 <template>
   <div>
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="form-group">
+          <label for="firstName">Prénom</label>
+          <input type="text" class="form-control" id="firstName" placeholder="John" v-model.lazy="user.firstName">
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="form-group">
+          <label for="lastName">Nom</label>
+          <input type="text" class="form-control" id="lastName" placeholder="Doe" v-model.lazy="user.lastName">
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="form-group">
+          <label for="phone">Téléphone</label>
+          <input type="tel" class="form-control" id="phone" placeholder="022 123 45 67" v-model.lazy="user.tel">
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" class="form-control" id="email" placeholder="nom@domain.ch" v-model.lazy="user.email">
+        </div>
+      </div>
+    </div>
+    <label for="message">Message</label>
+    <textarea class="form-control" v-model.lazy="message" id="message" style="margin-bottom:10px;" rows="10"></textarea>
+    <button class="btn btn-default btn-block" @click="sendMessage">Envoyer</button>
+    <div id="success-alert" class="alert alert-success" role="alert">
+      <i class="fa fa-check" aria-hidden="true"></i> Une personne vous contactera pour confirmer la reservation.
+    </div>
+    <div id="error-alert" class="alert alert-danger" role="alert">
+      <i class="fa fa-times" aria-hidden="true"></i>  {{error}}
+    </div>
   </div>
 </template>
 
@@ -7,6 +44,43 @@
 export default {
   data () {
     return {
+      user: {firstName: '', lastName: '', tel: '', email: ''},
+      message: '',
+      error: ''
+    }
+  },
+  mounted () {
+    $('#success-alert').hide()
+    $('#error-alert').hide()
+  },
+  methods: {
+    sendMessage () {
+      if (this.user.firstName === '' || this.user.lastName === '' || this.user.tel === '' || this.user.email === '') {
+        this.error = `Veuillez remplir le formulaire avant de l'envoyer.`
+        $('#error-alert').alert()
+        $('#error-alert').fadeTo(2000, 500).slideUp(500, function () {})
+        return
+      }
+      if (this.message.length < 100 || this.message.length > 2000) {
+        this.error = `Le message doit contenir entre 100 et 2000 caractères.`
+        $('#error-alert').alert()
+        $('#error-alert').fadeTo(2000, 500).slideUp(500, function () {})
+        return
+      }
+      const text = `Nom: ${this.user.firstName} ${this.user.lastName}
+Tel: ${this.user.tel} Email: ${this.user.email}
+Message: ${this.message}`
+      const self = this
+      this.$http.post(`${process.env.AWS_API_ROOT}contact`, {'data': text, 'subject': 'Contact', 'source': this.user.email})
+      .then((response) => {
+        this.message = ''
+        $('#success-alert').alert()
+        $('#success-alert').fadeTo(2000, 500).slideUp(500, function () {})
+      }, (response) => {
+        this.error = `Erreur, le message n'a pas pu être envoyé.`
+        $('#error-alert').alert()
+        $('#error-alert').fadeTo(2000, 500).slideUp(500, function () {})
+      })
     }
   }
 }
