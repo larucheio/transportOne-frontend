71a0e17ea6114e5969fe17c9ccfdb26444623667
maxirozay <rossier.maxime@gmail.com>
2016-11-14 22:36:43 +0100

    put the root API in the env variable

--- config/example.prod.env.js
+++ config/example.prod.env.js
@@ -3,5 +3,6 @@ module.exports = {
   AUTH0_CLIENT_ID: '"XYZ123ABC"',
   AUTH0_DOMAIN: '"xyz.eu.auth0.com"',
   PUBLIC_EMAIL: '"name@domain.com"',
-  SITE_URL: '"http://localhost:8080/#/"'
+  SITE_URL: '"http://localhost:8080/#/"',
+  AWS_API: '"https://*.execute-api.region.amazonaws.com/"'
 }
