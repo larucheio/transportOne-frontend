1b4a0f08bba0e83593eaed639cba2278ff871ea3
maxirozay <rossier.maxime@gmail.com>
2016-11-23 15:51:26 +0100

    create a button component to manage all the buttons

---
+++ src/components/Button.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="componentClass">
+    <div v-if="hasError" class="text-danger">{{error}}</div>
+    <button @click="clicked" :class="customClass">{{text}} <i :class="icon"></i></button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: 'Button'
+    },
+    error: {
+      type: String,
+      default: 'Error'
+    },
+    componentClass: {
+      type: String,
+      default: 'float-xs-right'
+    },
+    customClass: {
+      type: String,
+      default: 'btn btn-primary float-xs-right'
+    }
+  },
+  data () {
+    return {
+      hasError: false,
+      icon: null,
+    }
+  },
+  methods: {
+    showPending () {
+      this.hasError = false
+      this.icon = 'fa fa-spinner fa-pulse fa-fw'
+    },
+    showSuccess () {
+      this.icon = 'fa fa-check'
+      setTimeout(() => {this.icon = null}, 2000);
+    },
+    showError () {
+      this.hasError = true
+      this.icon = 'fa fa-times'
+      setTimeout(() => {this.icon = null}, 2000);
+    },
+    clicked () {
+      this.$emit('click')
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import VueResource from 'vue-resource'
 import './style/style.scss'
 import {router} from './router.js'
 import Input from './components/Input.vue'
+import Button from './components/Button.vue'
 Vue.use(VueResource)
 Vue.use(VueRouter)
 Vue.component('custom-input', Input)
+Vue.component('custom-button', Button)
 
 window.jQuery = require('jquery')
 window.$ = window.jQuery
