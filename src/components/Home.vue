56f2a791a1d1d525d3f5012f68bda63502d4e68b
maxirozay <rossier.maxime@gmail.com>
2017-01-18 13:43:23 +0100

    add spacing to the home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,30 +3,30 @@
     <h4 class="u-center-text mt-2">Transport accompagné de qualité avec chauffeur privé</h4>
     <h4 class="u-center-text mt-2">Prix le plus bas Garanti sur Genève <strong>dès 25CHF</strong></h4>
     <h4 class="u-center-text mt-2">Ponctuelle, serviable, disponible et sécurisé</h4>
-    <pricing></pricing>
-    <div class="row u-center-text">
+    <pricing class="mt-3"></pricing>
+    <div class="row u-center-text mt-3">
       <div class="col-sm-3">
         <img src="../assets/best_price.png" alt="Meilleur prix" height="150" width="150">
-        <h5 class="font-weight-bold">Garantie meilleurs prix</h5>
+        <h5 class="font-weight-bold mt-1">Garantie meilleurs prix</h5>
         <p>Faites des économies avec nos prix les plus bas de Genève. Nos tarifs sont fixes et garantis lors de la réservation.</p>
       </div>
       <div class="col-sm-3">
         <img src="../assets/free_cancellation.png" alt="Annulation gratuite" height="150" width="150">
-        <h5 class="font-weight-bold">Annulation gratuite</h5>
+        <h5 class="font-weight-bold mt-1">Annulation gratuite</h5>
         <p>Un changement de programme? aucun soucis. Vous disposez d&#39;une annulation gratuite avec un préavis de 24 heure. (par téléphone ou par mail).</p>
       </div>
       <div class="col-sm-3">
         <img src="../assets/service_in_time.png" alt="Service dans les temps" height="150" width="150">
-        <h5 class="font-weight-bold">Service dans les temps</h5>
+        <h5 class="font-weight-bold mt-1">Service dans les temps</h5>
         <p>Grace a notre connaissance minutieuse de Genève et ses environs, nous garantissons un service ponctuelle et dans les temps.</p>
       </div>
       <div class="col-sm-3">
         <img src="../assets/pro_driver.png" alt="Chauffeurs professionels" height="150" width="150">
-        <h5 class="font-weight-bold">Chauffeurs professionels</h5>
+        <h5 class="font-weight-bold mt-1">Chauffeurs professionels</h5>
         <p>Laissez vous transportez sereinement! L&#39;ensemble des chauffeurs Transport One est assuré et agréé. Nous garantissons un service soigné et de qualité.</p>
       </div>
     </div>
-    <h1>Nos services</h1>
+    <h1 class="mt-3">Nos services</h1>
     <p>Transport One permet à tout le monde d’être transportées et accompagnées durant leurs déplacements, tout en faisant des économies!
       Laissez vous donc transporter sereinement dans notre rayonnante ville de Genève et ses environs.</p>
     <p>Fondé en 1998, Transport One est une entreprise Familiale spécialisée dans le transport accompagné avec chauffeur privé.</p>
