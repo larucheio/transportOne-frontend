e1c8f5030fbe24465fb4ca949a0fd7b112e46f7c
maxirozay <rossier.maxime@gmail.com>
2017-01-18 12:45:51 +0100

    clean home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <h1>{{ title }}</h1>
-    <h6>{{ text }}</h6>
     <pricing></pricing>
   </div>
 </template>
@@ -9,34 +7,9 @@
 <script>
 import Pricing from './Pricing.vue'
 export default {
-  name: 'home',
-  data () {
-    return {
-      title: 'Transport One',
-      text: 'Transport One est...'
-    }
-  },
   components: { 'pricing': Pricing }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style>
 </style>
