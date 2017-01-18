da7f822cafb4276742a35de6bdc7460991763428
maxirozay <rossier.maxime@gmail.com>
2017-01-18 14:52:27 +0100

    correct typo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,41 +1,41 @@
 <template>
   <div>
     <h4 class="u-center-text mt-2">Transport accompagné de qualité avec chauffeur privé</h4>
-    <h4 class="u-center-text mt-2">Prix le plus bas Garanti sur Genève <strong>dès 25CHF</strong></h4>
+    <h4 class="u-center-text mt-2">Prix le plus bas garantie sur Genève <strong>dès 25CHF</strong></h4>
     <h4 class="u-center-text mt-2">Ponctuelle, serviable, disponible et sécurisé</h4>
     <pricing class="mt-3"></pricing>
     <div class="row u-center-text mt-3">
       <div class="col-md-3">
         <img src="../assets/best_price.png" alt="Meilleur prix" height="150" width="150">
         <h5 class="font-weight-bold mt-1">Garantie meilleurs prix</h5>
-        <p>Faites des économies avec nos prix les plus bas de Genève. Nos tarifs sont fixes et garantis lors de la réservation.</p>
+        <p>Faites des économies avec nos prix les plus bas de Genève. Nos tarifs sont fixes et garanties lors de la réservation.</p>
       </div>
       <div class="col-md-3">
         <img src="../assets/free_cancellation.png" alt="Annulation gratuite" height="150" width="150">
         <h5 class="font-weight-bold mt-1">Annulation gratuite</h5>
-        <p>Un changement de programme? aucun soucis. Vous disposez d&#39;une annulation gratuite avec un préavis de 24 heure. (par téléphone ou par mail).</p>
+        <p>Un changement de programme? aucun souci. Vous disposez d&#39;une annulation gratuite avec un préavis de 24 heure. (par téléphone ou par email).</p>
       </div>
       <div class="col-md-3">
         <img src="../assets/service_in_time.png" alt="Service dans les temps" height="150" width="150">
         <h5 class="font-weight-bold mt-1">Service dans les temps</h5>
-        <p>Grace a notre connaissance minutieuse de Genève et ses environs, nous garantissons un service ponctuelle et dans les temps.</p>
+        <p>Grace à notre connaissance minutieuse de Genève et ses environs, nous garantissons un service ponctuelle et dans les temps.</p>
       </div>
       <div class="col-md-3">
         <img src="../assets/pro_driver.png" alt="Chauffeurs professionels" height="150" width="150">
         <h5 class="font-weight-bold mt-1">Chauffeurs professionels</h5>
-        <p>Laissez vous transportez sereinement! L&#39;ensemble des chauffeurs Transport One est assuré et agréé. Nous garantissons un service soigné et de qualité.</p>
+        <p>Laissez-vous transporter sereinement! L&#39;ensemble des chauffeurs Transport One est assuré et agréé. Nous garantissons un service soigné et de qualité.</p>
       </div>
     </div>
     <h1 class="mt-3">Nos services</h1>
-    <p>Transport One permet à tout le monde d’être transportées et accompagnées durant leurs déplacements, tout en faisant des économies!
-      Laissez vous donc transporter sereinement dans notre rayonnante ville de Genève et ses environs.</p>
+    <p>Transport One permet à tout le monde d’être transporté et accompagné durant leurs déplacements, tout en faisant des économies!
+      Laissez-vous donc transporter sereinement dans notre rayonnante ville de Genève et ses environs.</p>
     <p>Fondé en 1998, Transport One est une entreprise Familiale spécialisée dans le transport accompagné avec chauffeur privé.</p>
-    <p>Notre engagement professionnelle garantit un service ponctuelle et de qualité ainsi qu’un accompagnement soigné et sécurisé.</p>
+    <p>Notre engagement professionnel garantit un service ponctuel et de qualité ainsi qu’un accompagnement soigné et sécurisé.</p>
     <p>Que ce soit pour:</p>
     <ul>
       <li>Rendez-vous médicaux</li>
       <li>Centre de bien-être</li>
-      <li>Ecole</li>
+      <li>École</li>
       <li>Rendez-vous privé</li>
       <li>Toute autre destination</li>
     </ul>
@@ -44,10 +44,10 @@
       <li>Personnes âgées</li>
       <li>Adultes</li>
       <li>Enfants</li>
-      <li>Groupe d’amis, familles, employé d’entreprise,...</li>
+      <li>Groupe d’amis, familles, employé d’entreprise...</li>
       <li>Avec ou sans mobilité réduite (personne en fauteuil roulant non inclus excepté si elle est dans la capacité d’être installé sur l’un de nos siege)</li>
     </ul>
-    <p>Nous avons mis en place un service “Commision” qui comprend un aller - retour à domicile tout en accompagnant la personne pour les commissions, la Poste ou autre demande requérant une aide.</p>
+    <p>Nous avons mis en place un service “Commision” qui comprend un aller-retour à domicile tout en accompagnant la personne pour les commissions, la Poste ou autre demande requérant une aide.</p>
     <p>Les destinations “Aéroport” et “Gare” avec accompagnement sont à votre disposition.</p>
   </div>
 </template>
