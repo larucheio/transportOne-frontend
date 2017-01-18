27fbd8dbb6e505ca913e0077d84a0ba86cd9356a
maxirozay <rossier.maxime@gmail.com>
2017-01-18 12:50:24 +0100

    add nos service text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,29 @@
 <template>
   <div>
     <pricing></pricing>
+    <h1>Nos services</h1>
+    <p>Transport One permet à tout le monde d’être transportées et accompagnées durant leurs déplacements, tout en faisant des économies!
+      Laissez vous donc transporter sereinement dans notre rayonnante ville de Genève et ses environs.</p>
+    <p>Fondé en 1998, Transport One est une entreprise Familiale spécialisée dans le transport accompagné avec chauffeur privé.</p>
+    <p>Notre engagement professionnelle garantit un service ponctuelle et de qualité ainsi qu’un accompagnement soigné et sécurisé.</p>
+    <p>Que ce soit pour:</p>
+    <ul>
+      <li>Rendez-vous médicaux</li>
+      <li>Centre de bien-être</li>
+      <li>Ecole</li>
+      <li>Rendez-vous privé</li>
+      <li>Toute autre destination</li>
+    </ul>
+    <p>Et que vous soyez:</p>
+    <ul>
+      <li>Personnes âgées</li>
+      <li>Adultes</li>
+      <li>Enfants</li>
+      <li>Groupe d’amis, familles, employé d’entreprise,...</li>
+      <li>Avec ou sans mobilité réduite (personne en fauteuil roulant non inclus excepté si elle est dans la capacité d’être installé sur l’un de nos siege)</li>
+    </ul>
+    <p>Nous avons mis en place un service “Commision” qui comprend un aller - retour à domicile tout en accompagnant la personne pour les commissions, la Poste ou autre demande requérant une aide.</p>
+    <p>Les destinations “Aéroport” et “Gare” avec accompagnement sont à votre disposition.</p>
   </div>
 </template>
 
