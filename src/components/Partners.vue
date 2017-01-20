79c1135fc285437e8e37b0420f24e71152fc9732
maxime rossier <rossier.maxime@gmail.com>
2017-01-20 20:26:06 +0100

    fix reviews with bootstrap alpha6

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -26,11 +26,14 @@ const Review = {
         <img :src="review.userPic" onerror="this.style.display='none'" class="rounded" style="margin-right:10px; margin-bottom:10px;">
       </div>
       <div class="media-body">
-        <strong>{{review.username}}</strong><small class="float-xs-right">{{formatedDate}}</small>
-        <span v-for="id in 5">
-          <i v-if="id > review.rate" class="fa fa-star-o fa-lg star" aria-hidden="true"></i>
-          <i v-else class="fa fa-star fa-lg star" aria-hidden="true"></i>
-        </span>
+        <div class="d-flex justify-content-start">
+          <strong  class="mr-2">{{review.username}}</strong>
+          <span v-for="id in 5">
+            <i v-if="id > review.rate" class="fa fa-star-o fa-lg star" aria-hidden="true"></i>
+            <i v-else class="fa fa-star fa-lg star" aria-hidden="true"></i>
+          </span>
+          <small class="ml-auto">{{formatedDate}}</small>
+        </div>
         <p>{{review.review}}</p>
         <custom-button v-if="showDeleteButton" ref="deleteReviewsButton" @click="deleteReview" text="Supprimer" pendingText="Supprime" successText="Supprimé" customClass="btn btn-danger float-xs-right morph"></custom-button>
         <custom-button v-if="showUndoButton" ref="undoButton" @click="setReview" text="Récupérer" pendingText="Récupère" successText="Récupéré"></custom-button>
