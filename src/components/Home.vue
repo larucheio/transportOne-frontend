2b884f71f0ffa1773a5362ab99a3c9f41d451ebb
maxime rossier <rossier.maxime@gmail.com>
2017-02-03 13:55:58 +0100

    change design details

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,22 +13,22 @@
         <div class="row text-center">
           <div class="col-md-3">
             <i class="fa fa-usd fa-5x" aria-hidden="true"></i>
-            <h5 class="font-weight-bold m-3">Garantie meilleurs prix</h5>
+            <h5 class="font-weight-bold m-3 text-uppercase">Garantie meilleurs prix</h5>
             <p>Faites des économies avec nos prix les plus bas de Genève. Nos tarifs sont fixes et garanties lors de la réservation.</p>
           </div>
           <div class="col-md-3">
             <i class="fa fa-hand-paper-o fa-5x" aria-hidden="true"></i>
-            <h5 class="font-weight-bold m-3">Annulation gratuite</h5>
+            <h5 class="font-weight-bold m-3 text-uppercase">Annulation gratuite</h5>
             <p>Un changement de programme? aucun souci. Vous disposez d&#39;une annulation gratuite avec un préavis de 24 heure. (par téléphone ou par email).</p>
           </div>
           <div class="col-md-3">
             <i class="fa fa-clock-o fa-5x" aria-hidden="true"></i>
-            <h5 class="font-weight-bold m-3">Service dans les temps</h5>
+            <h5 class="font-weight-bold m-3 text-uppercase">Service dans les temps</h5>
             <p>Grace à notre connaissance minutieuse de Genève et ses environs, nous garantissons un service ponctuelle et dans les temps.</p>
           </div>
           <div class="col-md-3">
             <i class="fa fa-id-card-o fa-5x" aria-hidden="true"></i>
-            <h5 class="font-weight-bold m-3">Chauffeurs professionels</h5>
+            <h5 class="font-weight-bold m-3 text-uppercase">Chauffeurs professionels</h5>
             <p>Laissez-vous transporter sereinement! L&#39;ensemble des chauffeurs Transport One est assuré et agréé. Nous garantissons un service soigné et de qualité.</p>
           </div>
         </div>
@@ -48,16 +48,6 @@
         <p>Notre engagement professionnel garantit un service ponctuel et de qualité ainsi qu’un accompagnement soigné et sécurisé.</p>
         <div class="row">
           <div class="col-md-6">
-            <p>Que ce soit pour:</p>
-            <ul class="fa-ul">
-              <li><i class="fa-li fa fa-map-marker"></i>Rendez-vous médicaux</li>
-              <li><i class="fa-li fa fa-map-marker"></i>Centre de bien-être</li>
-              <li><i class="fa-li fa fa-map-marker"></i>École</li>
-              <li><i class="fa-li fa fa-map-marker"></i>Rendez-vous privé</li>
-              <li><i class="fa-li fa fa-map-marker"></i>Toute autre destination</li>
-            </ul>
-          </div>
-          <div class="col-md-6">
             <p>Et que vous soyez:</p>
             <ul class="fa-ul">
               <li><i class="fa-li fa fa-users"></i>Personnes âgées</li>
@@ -65,6 +55,16 @@
               <li><i class="fa-li fa fa-users"></i>Enfants</li>
               <li><i class="fa-li fa fa-users"></i>Groupe d’amis, familles, employé d’entreprise...</li>
               <li><i class="fa-li fa fa-users"></i>Avec ou sans mobilité réduite (personne en fauteuil roulant non inclus excepté si elle est dans la capacité d’être installé sur l’un de nos siege)</li>
+            </ul>
+          </div>
+          <div class="col-md-6">
+            <p>Que ce soit pour:</p>
+            <ul class="fa-ul">
+              <li><i class="fa-li fa fa-map-marker"></i>Rendez-vous médicaux</li>
+              <li><i class="fa-li fa fa-map-marker"></i>Centre de bien-être</li>
+              <li><i class="fa-li fa fa-map-marker"></i>École</li>
+              <li><i class="fa-li fa fa-map-marker"></i>Rendez-vous privé</li>
+              <li><i class="fa-li fa fa-map-marker"></i>Toute autre destination</li>
             </ul>
           </div>
         </div>
