664c1724e145e7cb378518adc7535703382d50d3
maxime rossier <rossier.maxime@gmail.com>
2017-02-04 16:56:15 +0100

    edit footer

--- src/App.vue
+++ src/App.vue
@@ -40,18 +40,18 @@
     </transition>
     <footer class="bg-lightgrey mt-5">
       <div class="container">
-        <div class="row text-center align-items-center p-3">
+        <div class="row text-center align-items-center">
           <div class="col-lg-3 py-2">
-            <p>Copyright Transport One
-              <br>Tél: 079-200-28-28
-              <br>Email: email@domain.com
+            <p class="m-0">
+              <i class="fa fa-phone mr-1"></i>079-200-28-28
+              <br><i class="fa fa-envelope mr-1"></i><a href='mailto:email@domain.com' class="text-muted">email@domain.com</a>
             </p>
           </div>
           <div class="col-lg-3 py-2">
-            <a href='https://www.facebook.com'><img alt='Suivé nous sur Facebook' src='./assets/FB-f-Logo__blue_50.png' class=""/></a>
+            <a href='https://www.facebook.com'><img alt='Suivé nous sur Facebook' src='./assets/FB-f-Logo__blue_50.png'/></a>
           </div>
           <div class="col-lg-3 py-2">
-            <a href='https://play.google.com/store/apps/details?id=maxirozay.com.keymax&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Disponible sur Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/fr_badge_web_generic.png' class="google-play-link"/></a>
+            <a href='https://play.google.com/store/apps/details?id=maxirozay.com.keymax&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Disponible sur Google Play' src='./assets/google-play-badge.png'/></a>
           </div>
           <div class="col-lg-3 py-2">
             <a href="https://laruche.io" title="laruche - Agence web Suisse" class="text-muted">Réalisé à Genève par laruche.</a>
