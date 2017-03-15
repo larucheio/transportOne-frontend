d8f6ceb0f86138e19ad506eec878541151d8afe4
rspt <julien@rspt.ch>
2017-03-15 20:37:53 +0100

    Include config for ease of deployment

--- config/example.prod.env.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  AUTH0_CLIENT_ID: '"XYZ123ABC"',
-  AUTH0_DOMAIN: '"xyz.eu.auth0.com"',
-  PUBLIC_EMAIL: '"name@domain.com"',
-  SITE_URL: '"http://localhost:8080/#/"',
-  AWS_API: '"https://*.execute-api.region.amazonaws.com/"'
-}
