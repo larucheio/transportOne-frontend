b745afb019bf8a4b00332b15e500f901d359a87e
rspt <julien@rspt.ch>
2017-03-23 16:20:05 +0100

    Make phone clickable

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@
         <div class="row text-center align-items-center">
           <div class="col-lg-3 py-2">
             <p class="m-0">
-              <i class="fa fa-phone mr-1"></i>079 900 28 28
+              <i class="fa fa-phone mr-1"></i><a href="tel:+41799002828" class="text-muted">079 900 28 28</a>
               <br><i class="fa fa-envelope mr-1"></i><a href='mailto:info@transportone.ch' class="text-muted">info@transportone.ch</a>
             </p>
           </div>
