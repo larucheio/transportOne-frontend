1207eed5a80a68c34ab3ee8f848ec6b56e9d2366
maxime rossier <rossier.maxime@gmail.com>
2017-03-31 18:18:35 +0200

    remove # from links

--- src/components/HowItWorks.vue
+++ src/components/HowItWorks.vue
@@ -38,7 +38,7 @@
       <div class="col-md-6 py-2">
         <h2 class="text-uppercase">Réservation</h2>
         <p>Le détail et le prix de la course s’affichent automatiquement. II ne vous reste donc plus qu'à cliquer sur « <strong>Réserver</strong> ».</p>
-        <p><strong>Annulation gratuite</strong> : Vous disposez d'une annulation gratuite avec un préavis de 24 heures. <a href="#/contact" >Contactez-nous</a> par téléphone ou par email.</p>
+        <p><strong>Annulation gratuite</strong> : Vous disposez d'une annulation gratuite avec un préavis de 24 heures. <a href="/contact" >Contactez-nous</a> par téléphone ou par email.</p>
         <p>Nous vous invitons à cocher la case « <strong>Recevoir des offres par email</strong> » afin d’être averti de toutes nouveautés et modifications !</p>
       </div>
     </div>
@@ -46,7 +46,7 @@
       <div class="col-md-6 py-2">
         <img class="pb-3 mx-auto d-block hidden-md-up img-fluid" src="../assets/screenshot-login.jpg" alt="login">
         <h2 class="text-uppercase">Évaluez votre course</h2>
-        <p>Une fois votre course terminée, n’hésitez pas à nous donner votre avis. Pour ce faire, connectez-vous avec votre compte <strong>Facebook/Gmail</strong> ou créez tout simplement un compte Transport One et laissé votre avis sur la page <a href="#/partenaires">Paternaires &amp; avis</a>.</p>
+        <p>Une fois votre course terminée, n’hésitez pas à nous donner votre avis. Pour ce faire, connectez-vous avec votre compte <strong>Facebook/Gmail</strong> ou créez tout simplement un compte Transport One et laissé votre avis sur la page <a href="/partenaires">Paternaires &amp; avis</a>.</p>
         <p>Il vous suffit de noter et de laisser un commentaire sur la prestation et la qualité de notre service.</p>
         <p>Vous pouvez également évaluer votre course en laissant un avis sur notre page <strong>Facebook</strong>.</p>
       </div>
