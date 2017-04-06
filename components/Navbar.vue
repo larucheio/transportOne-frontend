f8823eb10c944c7972f0fc93cf5b587a01249331
rspt <julien@rspt.ch>
2017-04-06 14:13:58 +0200

    Put the nav in its own module

---
+++ components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <nuxt-link to="/">Accueil</nuxt-link>
+    <nuxt-link to="/partenaires">Partenaires</nuxt-link>
+    <nuxt-link v-if="isAdmin" to="/admin">Admin</nuxt-link>
+    <nuxt-link v-if="!isAuthenticated" to="/auth/sign-in">Sign In</nuxt-link>
+    <nuxt-link v-else to="/auth/sign-off">Sign Off</nuxt-link>
+  </header>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: mapGetters(['isAuthenticated', 'isAdmin'])
+}
+</script>
