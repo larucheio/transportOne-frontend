3c994f8b0515a693436fc46953543effcdb1aace
rspt <julien@rspt.ch>
2017-04-12 13:59:39 +0200

    Add Vehicule page

--- pages/vehicule/index.vue
+++ pages/vehicule/index.vue
@@ -1,5 +1,48 @@
 <template>
-  <section>
-    <h1>Véhicule</h1>
-  </section>
+  <div class="container mt-5">
+    <h2 class="text-uppercase mb-4">Véhicule et équipement</h2>
+    <div class="row py-3">
+      <div class="col-md-2 text-center">
+        <i class="fa fa-hand-paper-o fa-5x" aria-hidden="true"></i>
+      </div>
+      <div class="col-md-10">
+        <p class="my-4">Certaines entreprises de transport ne respectent pas la loi obligeant le conducteur à faire une pause et à ne pas travailler plus de certaines heures d’affilée, et cela aux dépens du client!</p>
+      </div>
+    </div>
+    <p>Chez Transport One, la durée de conduite <i class="fa fa-car" aria-hidden="true"></i> et la durée de repos <i class="fa fa-battery-quarter" aria-hidden="true"></i> sont primordiales et nous les appliquons à la lettre.</p>
+    <div class="row py-3">
+      <div class="col-md-6">
+        <p>C’est pourquoi afin de respecter minutieusement la loi « OTR2 » sur le Transport Professionnel de Personnes, tous nos véhicules sont équipés de tachygraphe.</p>
+      </div>
+      <div class="col-md-6">
+        <img src="~assets/img/vehicule/tachygraphe.jpg" class="img-fluid" alt="tachygraphe">
+      </div>
+    </div>
+    <div class="row py-3">
+      <div class="col-md-2 text-center">
+        <i class="fa fa-id-card fa-5x" aria-hidden="true"></i>
+      </div>
+      <div class="col-md-10">
+        <p class="my-4">De plus, l'ensemble de nos chauffeurs Transport One sont assurés et agréés.</p>
+      </div>
+    </div>
+    <h5 class="mt-5">Intérieur</h5>
+    <ul>
+      <li>8 places</li>
+      <li>Air climatisé</li>
+      <li>Coffre spacieux (ex. fauteuil roulant pliable)</li>
+      <li>Facilité d’accéder aux sièges</li>
+      <li>Équipement de sécurité, poignée de sécurité</li>
+      <li>Siège pour bébé</li>
+    </ul>
+    <div class="row py-3">
+      <div class="col-md-6">
+        <img src="~assets/img/vehicule/car.jpg" class="img-fluid" alt="véhicule">
+      </div>
+      <div class="col-md-6">
+        <img src="~assets/img/vehicule/car-inside.jpg" class="img-fluid" alt="intérieur du véhicule">
+      </div>
+    </div>
+    <h1 class="py-5 text-center">Laissez-vous donc transporter sereinement !</h1>
+  </div>
 </template>
