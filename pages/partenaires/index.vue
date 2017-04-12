9b65f7ecf9f777214386d55e5083abedffb59567
rspt <julien@rspt.ch>
2017-04-12 14:07:33 +0200

    Update Partners/reviews page

--- pages/partenaires/index.vue
+++ pages/partenaires/index.vue
@@ -1,33 +1,71 @@
 <template>
-  <div>
+  <div class="container mt-5">
     <section>
-      <h1>Partenaires</h1>
+      <h1 class="text-uppercase">Partenaires</h1>
       <p>
         Transport One est en collaboration avec de nombreux instituts, mairies,
         foyers, associations, fondations et autres clients privés.
       </p>
       <p>
-        N'hésitez pas à nous <a href="/contact" >contacter</a> pour toute demande
+        N'hésitez pas à nous <a href="/contact">contacter</a> pour toute demande
         de renseignements, devis ou autres informations concernant la mise en
         place de contrat fixe pour des transports journalier, hebdomadaire,
         mensuel ou sur toute l'année.
       </p>
       <p>
         Voici une sélection de nos fidèles partenaires avec qui nous travaillons
-        depuis plus de dix ans.
+        depuis plus de dix ans:
+      </p>
+
+      <div class="row">
+        <div class="col-sm-6">
+          <p><img src="~assets/img/partenaires/imad.gif" alt="IMAD" height="50" class="m-3">
+            <br>IMAD - institution genevoise de maintien à domicile
+            <br><i class="fa fa-phone mr-1"></i>022 420 20 00
+            <br><i class="fa fa-envelope mr-1"></i><a href='mailto:info@imad-ge.ch' class="text-muted">info@imad-ge.ch</a>
+          </p>
+        </div>
+        <div class="col-sm-6">
+          <p><img src="~assets/img/partenaires/collonge-bellerive.jpg" alt="collonge-bellerive" height="50" class="m-3">
+            <br>Mairie de Collonge-Bellerive
+            <br><i class="fa fa-phone mr-1"></i>022 722 11 50
+            <br><i class="fa fa-envelope mr-1"></i><a href='mailto:info@collonge-bellerive.ch' class="text-muted">info@collonge-bellerive.ch</a>
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-6">
+          <p><img src="~assets/img/partenaires/plo.png" alt="PLO" height="50" class="m-3">
+            <br>Mairie de Plan-Les-Ouates
+            <br><i class="fa fa-phone mr-1"></i>022 884 64 00
+            <br><i class="fa fa-envelope mr-1"></i><a href='mailto:info@plan-les-ouates.ch' class="text-muted">info@plan-les-ouates.ch</a>
+          </p>
+        </div>
+        <div class="col-sm-6">
+          <p><img src="~assets/img/partenaires/caroubier.svg" alt="caroubier" height="50" class="m-3">
+            <br>Foyer de jour « Le Carroubier »
+            <br><i class="fa fa-phone mr-1"></i>022 343 60 66
+            <br><i class="fa fa-envelope mr-1"></i><a href='mailto:caroubier@ge.pro-senectute.ch' class="text-muted">caroubier@ge.pro-senectute.ch</a>
+          </p>
+        </div>
+      </div>
+      <p><img src="~assets/img/partenaires/oasis.png" alt="oasis" height="50" class="m-3">
+        <br>Association Foyer de Jour l&#39;Oasis
+        <br><i class="fa fa-phone mr-1"></i>079 200 28 28
+        <br><i class="fa fa-envelope mr-1"></i><a href='mailto:oasis@ge.pro-senectute.ch' class="text-muted">oasis@ge.pro-senectute.ch</a>
       </p>
 
     </section>
     <section>
       <h1>Votre avis nous intéresse</h1>
       <p>
-        Soucieux d’améliorer nos services en continu, votre opinion permet à
+        Soucieux d’améliorer continuellement nos services, votre opinion permet à
         Transport One de s’impliquer totalement dans la dynamique client et de
         rester toujours au fait de vos préoccupations et de vos souhaits.
       </p>
       <p>
         C’est pourquoi nous vous encourageons vivement à noter la qualité de
-        votre transport et de nous laisser votre avis !
+        votre transport et à nous laisser votre avis !
       </p>
       <textarea name="name" rows="8" cols="80"></textarea>
       <button type="submit" name="button">Envoyer</button>
@@ -38,9 +76,3 @@
     </section>
   </div>
 </template>
-
-<script>
-</script>
-
-<style>
-</style>
